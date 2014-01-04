493477d831ac76f0261380c667ff65b9389f6c32
orangain <orangain@gmail.com>
2014-01-04 18:21:48 +0900

    startproject

---
+++ helloscrapy/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for helloscrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'helloscrapy'
+
+SPIDER_MODULES = ['helloscrapy.spiders']
+NEWSPIDER_MODULE = 'helloscrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'helloscrapy (+http://www.yourdomain.com)'
